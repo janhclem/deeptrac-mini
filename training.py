823a0fc412c21f04ab8fa7a26541910d12f8ff33
geospatial-nrw <geospatial.nrw@proton.me>
2026-05-10 08:09:50 +0200

    Added a training loop

--- training.py
+++ training.py
@@ -6,6 +6,7 @@ from torch_geometric.nn import radius_graph
 from torch_geometric.utils import k_hop_subgraph
 from torch_scatter import scatter_sum
 from glob import glob
+from tqdm import tqdm
 
 import deeptraclib as deeptrac
 import minitraclib as minitrac
@@ -19,57 +20,49 @@ atm1 = minitrac.Atm()
 
 FILES_OUT = "./out/*/*"
 files = glob(FILES_OUT)
+np.random.shuffle(files) # Randomize training data...
 
-data = np.load(files[0])
-atm0.x = data["x"]
-atm1.x = data["x"]
-atm0.m = data["m"]
-atm1.m = data["m_"]
+for f in tqdm(files):
 
-x = atm0.x
-m = atm0.m
+    # Read the data...
+    data = np.load(f)
+    atm0.x = data["x"]
+    atm1.x = data["x"]
+    atm0.m = data["m"]
+    atm1.m = data["m_"]
 
-# Construct global graph
-f = np.concatenate([x, m[:, None]], axis=1)  # Node features: [x, y, mass]
-f_graph = torch.from_numpy(f).float()
-pos_graph = torch.from_numpy(x).float()
+    # Construct global graph properties...
+    f = np.concatenate([atm0.x, atm0.m[:, None]], axis=1)  # Node features: [x, y, mass]
+    f_graph = torch.from_numpy(f).float()
+    pos_graph = torch.from_numpy(atm0.x).float()
 
-# Build global graph only connecting nearby particles...
-edge_index = radius_graph(pos_graph, r=cfg.lx, batch=None, loop=False)
+    # Build global graph only connecting nearby particles...
+    edge_index = radius_graph(pos_graph, r=cfg.lx, batch=None, loop=False)
 
-# Create global Data object
-data_graph = Data(
-    x=f_graph,          # Node features (x, y, mass)
-    edge_index=edge_index, # 
-    pos=pos_graph       # Spatial positions
-)
+    # Create global data object
+    data_graph = Data(
+        x=f_graph,          # Node features (x, y, mass)
+        edge_index=edge_index, #
+        pos=pos_graph       # Spatial positions
+    )
 
-# Create the edge attributes...
-i, j = data_graph.edge_index
-edge_attr = data_graph.x[j] - data_graph.x[i]  # Edge features: [dx, dy, dm]
-data_graph.edge_attr = edge_attr				   
-data_graph_local = data_graph
+    # Create the edge attributes...
+    i, j = data_graph.edge_index
+    edge_attr = data_graph.x[j] - data_graph.x[i]  # Edge features: [dx, dy, dm]
+    data_graph.edge_attr = edge_attr
+    data_graph_local = data_graph
 
-# Print info
-#mixlib.plot_graph(data_graph_local)
-print(f"Global graph: {data_graph.num_nodes} nodes, {data_graph.num_edges} edges")
-print(f"Local subgraph: {data_graph_local.num_nodes} nodes, {data_graph_local.num_edges} edges")
-print(f"Node features shape: {data_graph_local.x.shape}")
-print(f"Edge features shape: {data_graph_local.edge_attr.shape}")
-print(f"Position shape: {data_graph_local.pos.shape}")
+    deepmix = deeptrac.DeepMix(data_graph_local)
 
-deepmix = deeptrac.DeepMix(data_graph_local)
-print(deepmix)
+    # Select optimizer and loss function...
+    optimizer = torch.optim.Adam(deepmix.parameters(), lr=3e-4)
+    loss_fn = nn.MSELoss()
 
-# Select optimizer and loss function...
-optimizer = torch.optim.Adam(deepmix.parameters(), lr=3e-4)
-loss_fn = nn.MSELoss()
+    optimizer.zero_grad() # Reset optimizer...
+    _, m1 = deepmix(data_graph_local) # Forward propagation...
+    loss = loss_fn(m1, torch.from_numpy(atm1.m).float()) # Calculate loss...
+    loss.backward() # Backward propagation...
+    optimizer.step() # Make the optimization step...
 
-optimizer.zero_grad() # Reset optimizer...
-_, m1 = deepmix(data_graph_local) # Forward propagation...
-loss = loss_fn(m1, torch.from_numpy(atm1.m).float()) # Calculate loss...
-loss.backward() # Backward propagation...
-optimizer.step() # Make the optimization step...
+    print(loss)
 
-print(loss)
-		
