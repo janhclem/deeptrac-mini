e8de3e0427992c725014bf464e33f8585503612c
janhclem <j.clemens@fz-juelich.de>
2026-05-20 14:02:21 +0200

    Cleaned up training script

--- start_training.sh
+++ start_training.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-cd /home/icg130/Schreibtisch/deeptrac-mini
-nohup python training.py > training_output.log 2>&1 &
-echo "Training started. PID: $!"
-echo "Output: training_output.log"
-echo "Log: training.log"
+python training.py
+
